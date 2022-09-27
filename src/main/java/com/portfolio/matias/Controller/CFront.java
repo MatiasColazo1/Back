aeff254abfb668153b2c8c6443e1332f46c0ce8e
MatiasColazo1 <maty_colazo@hotmail.com>
2022-09-27 19:56:47 -0300

    corrigiendo CORS

--- src/main/java/com/portfolio/matias/Controller/CFront.java
+++ src/main/java/com/portfolio/matias/Controller/CFront.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/front")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendmatias.web.app")
 public class CFront {
     @Autowired
     SFront sFront;
