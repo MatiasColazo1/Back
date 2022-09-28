e0d43a9c47cec498db1c13d0256fa08e91157fb5
MatiasColazo1 <maty_colazo@hotmail.com>
2022-09-28 12:14:50 -0300

    CORS personas

--- src/main/java/com/portfolio/matias/Controller/PersonaController.java
+++ src/main/java/com/portfolio/matias/Controller/PersonaController.java
@@ -12,10 +12,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "https://frontendmatias.web.app")
+@RequestMapping("/personas")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
